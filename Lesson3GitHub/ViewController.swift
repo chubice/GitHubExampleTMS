669b38cc07eda572dde222dab8cde996792f264a
chubice <alshcherbinskiy92@gmail.com>
2025-03-05 14:27:22 +0300

    Initial Commit

---
+++ Lesson3GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3GitHub
+//
+//  Created by MacOS on 5.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
